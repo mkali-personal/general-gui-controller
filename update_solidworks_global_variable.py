f374ebf9b06fc0dfe5dae7f08469d6488071a56b
michaeka <michael.kali@weizmann.ac.il>
2025-10-19 15:54:07 +0300

    small change

--- update_solidworks_global_variable.py
+++ update_solidworks_global_variable.py
@@ -3,26 +3,32 @@ from core.general_gui_controller import *
 equations_path = r"C:\Users\michaeka\Weizmann Institute Dropbox\Michael Kali\Labs Dropbox\Laser Phase Plate\Designs\Optomechanics\Parts\equations.txt"
 SHORT_SLEEP = 0.1
 # %%
-# record_gui_template('solidworks - second open file three dots - 1920X1200.png')
+# record_gui_template('solidworks - solidworks equations file type - 1920X1200')
 
 # %%
-detect_template_and_act(r"solidworks - search bar - 1920X1200.png", relative_position=(1.386, 0.674), value_to_paste='eq', sleep_after_action=SHORT_SLEEP)
+detect_template_and_act(r"solidworks - search bar", relative_position=(1.386, 0.674), value_to_paste='eq', sleep_after_action=SHORT_SLEEP)
 
-detect_template_and_act(r"solidworks - equations - 1920X1200.png", sleep_after_action=SHORT_SLEEP)
+detect_template_and_act(r"solidworks - equations", sleep_after_action=SHORT_SLEEP)
 
-three_dots_position = detect_template_and_act(r"solidworks - open file three dots - 1920X1200.png", relative_position=(0.233, 0.388), sleep_after_action=SHORT_SLEEP, max_waiting_time_seconds=2)
+detect_template(r"solidworks - angular units label")
+
+three_dots_position = detect_template_and_act(r"solidworks - open file three dots", relative_position=(0.233, 0.388), sleep_after_action=SHORT_SLEEP, max_waiting_time_seconds=0.5)
 
 if three_dots_position is None:
-    detect_template_and_act(r"solidworks - link to external file checkbox - 1920X1200.png", relative_position=(0.405, 0.579))
+    detect_template_and_act(r"solidworks - link to external file checkbox", relative_position=(0.405, 0.579))
 
-detect_template_and_act(r"solidworks - second open file three dots - 1920X1200.png", relative_position=(0.508, 0.160))
-paste_value(equations_path)
+detect_template_and_act(r"solidworks - second open file three dots", relative_position=(0.508, 0.160))
 
+detect_template(r"solidworks - solidworks equations file type")
+
+sleep(SHORT_SLEEP)
+pyautogui.write(equations_path)
+sleep(SHORT_SLEEP)
 pyautogui.press('enter')
 sleep(SHORT_SLEEP)
 
-detect_template_and_act(r"solidworks - link file - 1920X1200.png", relative_position=(0.548, 0.592), sleep_after_action=SHORT_SLEEP)
+detect_template_and_act(r"solidworks - link button", relative_position=(0.548, 0.592), sleep_after_action=SHORT_SLEEP)
 
-wait_for_template_to_disappear("solidworks - link file - 1920X1200.png")
+# wait_for_template_to_disappear("solidworks - link button")
 
-detect_template_and_act(r"solidworks - ok button - 1920X1200.png", relative_position=(0.461, 0.745))
+# detect_template_and_act(r"solidworks - ok button", relative_position=(0.461, 0.745))
